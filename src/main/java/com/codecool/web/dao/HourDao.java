33bcf11c67fc1a70743145299d2d70468914c70b
Kenezke1 <herczeg.kenya2@gmail.com>
2018-05-03 13:40:12 +0200

    IMPLEMENT hourdao interface

--- src/main/java/com/codecool/web/dao/HourDao.java
+++ src/main/java/com/codecool/web/dao/HourDao.java
@@ -1,4 +1,17 @@
 package com.codecool.web.dao;
 
+import com.codecool.web.model.Hour;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface HourDao {
+
+    void addHour(int day_id, int value) throws SQLException;
+
+    void deleteHour(int id) throws SQLException;
+
+    Hour findHourById(int id) throws SQLException;
+
+    List<Hour> findHourByUserId(int userId) throws SQLException;
 }
