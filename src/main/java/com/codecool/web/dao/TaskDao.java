4de5ed830febee961b56fc94849dfaf41d53d272
Kenezke1 <herczeg.kenya2@gmail.com>
2018-05-03 13:40:32 +0200

    IMPLEMENT taskdao interface

--- src/main/java/com/codecool/web/dao/TaskDao.java
+++ src/main/java/com/codecool/web/dao/TaskDao.java
@@ -1,4 +1,23 @@
 package com.codecool.web.dao;
 
+import com.codecool.web.model.Task;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface TaskDao {
+
+    void addTask(int userId, String title, String description) throws SQLException;
+
+    void deleteTask(int id) throws SQLException;
+
+    void updateTaskTitle(int id, String newTitle) throws SQLException;
+
+    void updateTaskDescription(int id, String newDescription) throws SQLException;
+
+    Task findTaskById(int id) throws SQLException;
+
+    List<Task> findTaskByUserId(int userId) throws SQLException;
+
+    List<Task> findAllTask() throws SQLException;
 }
