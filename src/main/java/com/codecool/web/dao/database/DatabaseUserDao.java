3715281dea1fc68862f709c95e87013743601fc4
daviddude <uszkai.d@gmail.com>
2018-05-03 14:37:22 +0200

    implement all methods in DatabaseUserDao

--- src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
+++ src/main/java/com/codecool/web/dao/database/DatabaseUserDao.java
@@ -3,8 +3,7 @@ package com.codecool.web.dao.database;
 import com.codecool.web.dao.UserDao;
 import com.codecool.web.model.User;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,22 +15,77 @@ public class DatabaseUserDao  extends AbstractDao implements UserDao {
     }
 
     @Override
-    public void insertUser(String email, String user_name, String password, String role) throws SQLException {
-
+    public void insertUser(String email, String userName, String password, String role) throws SQLException {
+        if (email == null || "".equals(email)) {
+            throw new IllegalArgumentException("Email cannot be null or empty");
+        }
+        boolean autoCommit = connection.getAutoCommit();
+        connection.setAutoCommit(false);
+        String sql = "INSERT INTO app_user (email, user_name, password, role) VALUES (?,?,?,?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, email);
+            statement.setString(2, userName);
+            statement.setString(3, password);
+            statement.setString(4, role);
+            executeInsert(statement);
+        } catch (SQLException ex) {
+            connection.rollback();
+            throw ex;
+        } finally {
+            connection.setAutoCommit(autoCommit);
+        }
     }
 
     @Override
     public User find(String email) throws SQLException {
+        String sql = "Select * from app_user\n" +
+                " where email = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, email);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return fetchUser(resultSet);
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public User find(String email, String password) throws SQLException {
+        String sql = "Select * from app_user\n" +
+                " where email = ? and password = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, email);
+            statement.setString(2, password);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return fetchUser(resultSet);
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public List<User> findAll() throws SQLException {
-        return null;
+        List<User> users = new ArrayList<>();
+        String sql = "Select * from app_user";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                users.add(fetchUser(resultSet));
+            }
+        }
+        return users;
+    }
+
+    private User fetchUser(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("id");
+        String email = resultSet.getString("email");
+        String userName = resultSet.getString("user_name");
+        String password = resultSet.getString("password");
+        String role = resultSet.getString("role");
+        return new User(id, email, userName, password, role);
     }
 }
