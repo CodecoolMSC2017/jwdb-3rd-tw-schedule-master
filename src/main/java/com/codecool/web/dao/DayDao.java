7c1aa64361f3304de78c8f80411024467df75b99
Kenezke1 <herczeg.kenya2@gmail.com>
2018-05-03 13:39:52 +0200

    IMPLEMENT daydao interface

--- src/main/java/com/codecool/web/dao/DayDao.java
+++ src/main/java/com/codecool/web/dao/DayDao.java
@@ -1,4 +1,19 @@
 package com.codecool.web.dao;
 
+import com.codecool.web.model.Day;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface DayDao {
+
+    void addDay(int scheduleId, String title) throws SQLException;
+
+    void deleteDay(int id) throws SQLException;
+
+    void updateDay(int id, String newTitle) throws SQLException;
+
+    Day findDayById(int id) throws SQLException;
+
+    List<Day> findDayByScheduleId(int scheduleId) throws SQLException;
 }
