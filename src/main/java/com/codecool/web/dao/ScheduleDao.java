7e1673e4f1234306263e3131d3a95c7ac6ba5bbd
daviddude <uszkai.d@gmail.com>
2018-05-03 13:38:57 +0200

    ScheduleDao interface implemented

--- src/main/java/com/codecool/web/dao/ScheduleDao.java
+++ src/main/java/com/codecool/web/dao/ScheduleDao.java
@@ -1,4 +1,25 @@
-package com.codecool.web.dao.database;
+package com.codecool.web.dao;
+
+import com.codecool.web.model.Schedule;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface ScheduleDao {
+
+    void insertSchedule(int app_user_id, String title, String description) throws SQLException;
+
+    void deleteSchedule(int id) throws SQLException;
+
+    void updateTitle(int id, String title) throws SQLException;
+
+    void updateDescription(int id, String description) throws SQLException;
+
+    Schedule findById(int id) throws SQLException;
+
+    ArrayList<Schedule> findAllByUserId(int id) throws SQLException;
+
+    ArrayList<Schedule> findall() throws SQLException;
+
+
 }
