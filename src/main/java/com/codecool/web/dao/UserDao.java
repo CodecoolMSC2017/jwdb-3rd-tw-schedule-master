e665e2b6a6fef687da1319f8f5cbc8347991c0ea
daviddude <uszkai.d@gmail.com>
2018-05-03 13:38:36 +0200

    UserDao interface implemented

--- src/main/java/com/codecool/web/dao/UserDao.java
+++ src/main/java/com/codecool/web/dao/UserDao.java
@@ -1,4 +1,17 @@
-package com.codecool.web.dao.database;
+package com.codecool.web.dao;
+
+import com.codecool.web.model.User;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface UserDao {
+
+    void insertUser(String email, String user_name, String password, String role) throws SQLException;
+
+    User find(String email) throws SQLException;
+
+    User find(String email, String password) throws SQLException;
+
+    ArrayList<User> findAll() throws SQLException;
 }
