ab34ade33f4b075f14903b18ec9428c4344f60f7
Kenezke1 <herczeg.kenya2@gmail.com>
2018-05-28 14:40:47 +0200

    ADD custom message to custom exception

--- src/main/java/com/codecool/web/exception/AlreadyRegisteredException.java
+++ src/main/java/com/codecool/web/exception/AlreadyRegisteredException.java
@@ -1,4 +1,7 @@
 package com.codecool.web.exception;
 
 public class AlreadyRegisteredException extends Exception {
+    public AlreadyRegisteredException() {
+        super("User already registered");
+    }
 }
