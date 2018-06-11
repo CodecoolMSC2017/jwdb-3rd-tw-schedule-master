8c4a703b88d9558cdf01ce6cc2920c4aafe1f734
norcs <norbertcs@me.com>
2018-06-11 11:58:29 +0200

    Add missing services

--- src/main/java/com/codecool/web/servlet/GoogleLoginServlet.java
+++ src/main/java/com/codecool/web/servlet/GoogleLoginServlet.java
@@ -1,5 +1,16 @@
 package com.codecool.web.servlet;
 
+import com.codecool.web.dto.UserDto;
+import com.codecool.web.model.Schedule;
+import com.codecool.web.model.Task;
+import com.codecool.web.model.User;
+import com.codecool.web.service.ScheduleService;
+import com.codecool.web.service.TaskService;
+import com.codecool.web.service.UserService;
+import com.codecool.web.service.simple.SimpleScheduleService;
+import com.codecool.web.service.simple.SimpleTaskService;
+import com.codecool.web.service.simple.SimpleUserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,23 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Collections;
 import java.util.List;
-
-import com.codecool.web.dao.UserDao;
-import com.codecool.web.dto.UserDto;
-import com.codecool.web.model.Schedule;
-import com.codecool.web.model.Task;
-import com.codecool.web.model.User;
-import com.codecool.web.service.UserService;
-import com.codecool.web.service.simple.SimpleUserService;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.LowLevelHttpRequest;
-import com.google.api.client.http.apache.ApacheHttpTransport;
-import com.google.api.client.json.JsonFactory;
 
 
 @WebServlet("/googleLogin")
@@ -35,11 +30,13 @@ public class GoogleLoginServlet extends AbstractServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         try(Connection connection = getConnection(req.getServletContext())) {
             String email = req.getParameter("email");
             String name = req.getParameter("name");
             UserService userService = new SimpleUserService(connection);
+            TaskService taskService = new SimpleTaskService(connection);
+            ScheduleService scheduleService = new SimpleScheduleService(connection);
 
             User user = userService.connectWithGoogle(name, email);
             req.getSession().setAttribute("user", user);
